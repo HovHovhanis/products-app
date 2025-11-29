c2a434dc8d0bff9ae8e8f92e58dc8c571549da07
Hovhannes <hhovhannisyan348@gmail.com>
2025-11-29 13:51:32 +0400

    new projects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["fakestoreapi.com"], // добавляем домен для внешних картинок
+  },
 };
 
 export default nextConfig;
